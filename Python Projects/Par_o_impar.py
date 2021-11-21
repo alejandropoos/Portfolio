cabb9e51278bcc48a9ba58e6334cb7b2f4d1b0e1
Alejandro Poós <65425766+alejandropoos@users.noreply.github.com>
2021-11-21 16:38:55 -0300

    Create Par_o_impar.py

---
+++ Python Projects/Par_o_impar.py
@@ -0,0 +1,23 @@
+def main():
+    entrada = input(f"Por favor, ingrese un numero: ")
+    salir = "exit"
+    if entrada == salir:
+        print("Hasta luego!")
+        pass
+    else:
+        try:
+            modulo = int(entrada)%2
+            if modulo == 0:
+                estado = "par"
+            elif modulo != 0:
+                estado = "impar"
+            print(estado)
+            main()
+        except TypeError:
+            print("Atencion! Ingrese únicamente números...")
+            main()  
+        except ValueError:
+            print("Atencion! Ingrese únicamente números enteros...")
+            main()
+
+main()
