2c1fe87d459957562a47858bc9647c408ade9e41
Alejandro Poós <65425766+alejandropoos@users.noreply.github.com>
2021-11-21 22:29:12 -0300

    Update bot_whatsapp.py

--- Python Projects/bot_whatsapp.py
+++ Python Projects/bot_whatsapp.py
@@ -2,7 +2,7 @@ import pyautogui
 import webbrowser
 from time import sleep
 
-webbrowser.open("https://web.whatsapp.com/send?phone=+543435199006")
+webbrowser.open("https://web.whatsapp.com/send?phone=+54xxxxxxxxx")
 sleep(10)
-pyautogui.typewrite("Hola estoy es un mensaje automatico?\n")
+pyautogui.typewrite("Hola! esto es un mensaje automatico!\n")
 pyautogui.press("enter")
