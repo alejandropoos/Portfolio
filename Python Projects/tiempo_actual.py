f555d0d3d8826866fde9b9d801498cac0a6592f3
Alejandro Poós <65425766+alejandropoos@users.noreply.github.com>
2021-11-22 13:26:08 -0300

    limpieza de código

--- Python Projects/tiempo_actual.py
+++ Python Projects/tiempo_actual.py
@@ -3,6 +3,6 @@ import requests
 URL = 'http://dataservice.accuweather.com/locations/v1/regions'
 data = requests.get(URL)
 datas = list()
-for element in data: #iteramos sobre data
+for element in data:
     datas.append(element)
 print(datas)
