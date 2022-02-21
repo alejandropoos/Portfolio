27b0d26e58f92fb964ddc29bc7ba9db862dd9d23
Alejandro Poós <65425766+alejandropoos@users.noreply.github.com>
2022-02-21 02:36:04 -0300

    Delete tiempo_actual.py

--- Python_Projects/tiempo_actual.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-URL = 'http://dataservice.accuweather.com/locations/v1/regions'
-data = requests.get(URL)
-datas = list()
-for element in data:
-    datas.append(element)
-print(datas)
